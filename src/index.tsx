2c001e5fdaaa217aa0d0ae36f94258ebcd0354da
Vinicius <ewerton.viniciusturco@gmail.com>
2023-11-19 17:58:36 -0300

    refactor: screen improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FlatList } from "react-native";
-import { XStack } from "tamagui";
+import { Spinner, XStack, Stack } from "tamagui";
+
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { mainActions as actions } from "./store/main/slice";
+import { getAllCharacter, getLoading } from "./store/main/selector";
 import { CardItem } from "./component/card";
-import data from "./services/mock/mock";
 
 const Main = () => {
+  const dispatch = useAppDispatch();
+  const characters = useAppSelector(getAllCharacter);
+  const loading = useAppSelector(getLoading);
+
+  useEffect(() => {
+    dispatch(actions.getAllCharacter());
+  }, [dispatch]);
+
+  if (loading === "loading") {
+    return (
+      <Stack f={1} ai={"center"} jc={"center"}>
+        <Spinner size="large" color="$orange10" />
+      </Stack>
+    );
+  }
+
   return (
     <XStack f={1} ai={"center"}>
       <FlatList
-        data={data}
-        renderItem={(item) => <CardItem key={item.index} />}
+        data={characters}
+        renderItem={({ item }) => (
+          <CardItem
+            key={item._id}
+            name={item.name}
+            age={item.age}
+            race={item.race}
+            gender={item.gender}
+            height={item.height}
+            job={item.job}
+            rank={item.rank}
+            status={item.status}
+            image={item.image}
+          />
+        )}
         horizontal={true}
+        showsHorizontalScrollIndicator={false}
       />
     </XStack>
   );
